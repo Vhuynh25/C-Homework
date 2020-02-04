13c269dbe6b6f6d1302a89603c72d7ded70f440e
Vhuynh25 <43254910+Vhuynh25@users.noreply.github.com>
2020-02-03 17:50:43 -0800

    Add files via upload

--- 1-22.c
+++ 1-22.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+bool openFiles(int argc, const char* argv[]){
+  FILE** pfin;
+  FILE** pfout;
+  int* n ;
+  if (argc != 4){return FALSE;}
+  *pfin = fopen(argv[1], "r");
+  
+  if (fin == NULL){
+    fprintf(stderr, "Input file could not be opened");
+    return FALSE;
+  }
+  *fout = fopen(argv[2],"w");
+  if (fout == NULL){
+    fprintf(stderr, "Output file could not be opened");
+    fclose ()
+    return FALSE;
+  }
+  return TRUE;
+}
 
 int main(int argc,const char* argv[]){
-  if (argc != 4){fprintf(stderr, "Usage: ./fold inputfile outputfile int\n");}
+  if (openFiles(argc,argv[]) != TRUE){
+    fprintf(stderr, "Usage: ./fold inputfile outputfile int\n");
+    return 1;
+  }
   FILE* fin = fopen(argv[1],"r");
   FILE* fout = fopen(argv[2],"w");
   int count = 0;
   int t = atoi(argv[3]);
   
-  if (fin == NULL){
-    fprintf(stderr, "Input file could not be opened");
-    return 1;
-  }
-  if (fout == NULL){
-    fprintf(stderr, "Output file could not be opened");
-    return 1;
-  }
-
   char c;
   while((c = fgetc(fin)) != EOF){
     if (count % t == t-1 && c != '\n'){
