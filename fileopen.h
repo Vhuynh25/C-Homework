0c61ea1e7eeea5d58b5b11468b6fe294cb7c5e09
Vhuynh25 <43254910+Vhuynh25@users.noreply.github.com>
2020-02-04 13:12:52 -0800

    Add files via upload

---
+++ fileopen.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdio.h>
+#include <stdbool.h>
+
+bool openFiles(int argc, const char* argv[]) {
+	FILE** pfin;
+	FILE** pfout;
+	int* n;
+	if (argc != 4) { return false; }
+	*pfin = fopen(argv[1], "r");
+
+	if (pfin == NULL) {
+		fprintf(stderr, "Input file could not be opened");
+		return false;
+	}
+	*pfout = fopen(argv[2], "w");
+	if (pfout == NULL) {
+		fprintf(stderr, "Output file could not be opened");
+		fclose(*pfin);
+		return false;
+	}
+	return false;
+}
